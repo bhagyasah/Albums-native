84c99bf4494c4be05be637cb03cdf96cbdd9ea92
neptech4u <neptech4u@gmail.com>
2018-10-20 00:17:47 +0545

    basic structure file and related tool is installed

--- README.md
+++
@@ -1,5 +0,0 @@
-some text added for feature branch
-## purpose
-adding some extra feature localy 11:26
-
-one done in read me
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/** @format */
+// Import a library to help create component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a Component
+const App = () => (
+  <Text> Some Text </Text>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+// Render it to the device
+AppRegistry.registerComponent('helloworld', () => App);
